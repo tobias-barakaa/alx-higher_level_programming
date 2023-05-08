5114cc1f8b729c628fae8575ad75b13ad771cf2a
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-05-09 00:15:14 +0300

    Update 6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-print(f"Welcome to {(str1) + (str2)}!")
+str1 = "Welcome to"
+str2 = "Holberton"
+str3 = "School!"
+print(str1 + str2 + str3)
