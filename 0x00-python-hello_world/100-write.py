84c5dac60af2724dc46a4b39f20cfe86817e8d8a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-09 02:07:47 -0700

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 import sys
 
+
 def exit_with(msg):
     sys.stderr.write(msg + "\n")
     sys.exit(1)
+
 
 exit_with("and that piece of art is useful - Dora Korpar, 2015-10-19")
