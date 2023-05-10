ecf67cee71bf9338fdc6765ed087f61a1b34f3a3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-10 13:14:58 -0700

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split(" ")[5][:-1], str.split(" ")[6], str.split(" ")[-4], str.split(" ")[-3], str.split(" ")[-2], str.split(" ")[-1][:-1])
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
