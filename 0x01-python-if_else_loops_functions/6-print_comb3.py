a59797521f924c5584230564e1aaa511f858a254
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-10 13:39:17 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
+#!/usr/bin/python3
+
 for i in range(10):
-    for j in range(i + 1, 10):
-        print("{:02d}, {:02d}".format(i, j), end="")
-        if i < 8 or j < 9:
-            print(", ", end="")
-print()
+    for j in range(i+1, 10):
+        print("{}, {}".format(i, j))
