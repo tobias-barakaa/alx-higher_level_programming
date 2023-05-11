06a785fba0711aa32711828cb3b9ed86b957bc10
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-11 12:06:05 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     a, b = 10, 5
-    result = "{} {} {} {} {} {}".format(a, b, add(a, b), mul(a, b), sub(a, b), div(a, b))
-    print(result)
+    results = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
+    ops = ["+", "-", "*", "/"]
+    for op, res in zip(ops, results):
+        print("{} {} {} = {}".format(a, op, b, res))
