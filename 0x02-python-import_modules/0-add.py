a4cb0f43df803f009723001b149cf65df7f37b92
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-11 08:35:11 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,16 @@
-#function imported
+
+#!/usr/bin/python3
 
 from add_0 import add
 
-# Define the values of a and b
 a = 1
 b = 2
-# summation declaration
+
 result = add(a, b)
 
-#print functions
 print("{} + {} = {}".format(a, b, result))
+
+
+if __name__ == "__main__":
+    add()
+
