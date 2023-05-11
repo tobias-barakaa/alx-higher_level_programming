a8065373661859ec5bf0148aa5bd73d20b42adc2
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-11 12:46:03 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     a, b = 10, 5
     results = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
     ops = ["+", "-", "*", "/"]
-    for op, res in zip(ops, results):
-        print("{} {} {} = {}".format(a, op, b, res))
+    for i in range(len(results)):
+        print("{} {} {} = {}".format(a, ops[i], b, results[i]))
+    for op, func in zip(ops, [add, sub, mul, div]):
+        print("{} {} {} = {}".format(a, op, b, func(a, b)))
