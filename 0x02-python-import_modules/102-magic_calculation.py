1e475c7f894b4e04f6fa114522e51ede1a6ae551
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-11 14:55:37 -0700

    103-fast_alphabet.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    add, sub = __import__('magic_calculation_102', globals(), locals(), ['add', 'sub'], 0)
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 7):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
