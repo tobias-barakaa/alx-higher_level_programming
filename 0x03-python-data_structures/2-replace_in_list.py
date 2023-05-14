6901385a697e246e73d20b523a33bfc8364209b4
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-13 23:17:08 -0700

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(mylist):
+        return my_list
+    else:
+        my_list[idx] = element
+        my_list[idx] = element
+    return my_list
