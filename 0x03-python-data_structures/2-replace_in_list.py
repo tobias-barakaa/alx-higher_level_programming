0759df312b0ef4445217d15a99a8819dd0326fb9
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-13 23:23:07 -0700

    qw

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(mylist):
         return my_list
-    else:
-        my_list[idx] = element
+    for i in range(len(my_list)):
+        if my_list[i] == element:
+            my_list[idx] = element
     return my_list
