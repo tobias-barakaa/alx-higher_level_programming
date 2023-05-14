3d8f2665ff3eb1efb94f983ab1eec0a8b50253ba
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-13 23:48:48 -0700

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 
-def reverse_list(my_list=[]):
-    left = 0
-    right = len(my_list) - 1
-    while left < right:
-        # Swap
-        temp = my_list[left]
-        my_list[left] = my_list[right]
-        my_list[right] = temp
-        left += 1
-        right -= 1
+def print_reversed_list_integer(my_list=[]):
+    new_list = []
+    for x in reversed(my_list):
+     new_list.append(x)
 
-    return my_list
-
-my_list = [1, 2, 3, 4, 5]
-print(reverse_list(my_list))
+    print(new_list)
