0df560a3d74df05a377d3a1a31c9b355a6ba64b1
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-19 05:25:02 -0700

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    	if i in a_dictionary:
-        del a_dictionary[i]
+    	if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
