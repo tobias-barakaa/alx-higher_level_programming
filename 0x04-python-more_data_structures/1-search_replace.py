dbb56cff3e4518f20e97607ae518d2a11f5234bd
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-19 02:04:48 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,5 @@
 def search_replace(my_list, search, replace):
     for index, item in enumerate(my_list):
         if isinstance(item, list):
-            search_replace(item, search, replace)
-        else:
-            if item == search:
+            if index == search:
                 my_list[index] = replace
