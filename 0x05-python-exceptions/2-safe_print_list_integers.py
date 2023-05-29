533c10a3847f016aa93538ecf126097faa30d0e8
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-29 09:59:40 -0700

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    Prints integers from a list
+    Returns the count of printed integers
+    """
+    count = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+            elif type(x) == int and x > count:
+                raise OverflowError
+            else:
+                break
+    except OverflowError as e:
+        return e
+    finally:
+        print("")
+    return count
