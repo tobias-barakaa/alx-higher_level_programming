c7392ee3eed2bf1fec6ad5a5ef969a69ad8999df
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-29 10:07:26 -0700

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,8 +8,8 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end="")
+            if type((my_list[i]) == int):
+                print("{:d}". format(my_list[i]), end="")
                 count += 1
             elif type(x) == int and x > count:
                 raise OverflowError
