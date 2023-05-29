289b1106ccb29659c1c200297323c99ce44139ae
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-29 04:56:25 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     try:
-        summ = 0
+        counting = 0
         for i in my_list:
-            if summ < x:
+            counting += 1
+            if counting <= x:
                 print(i, end=" ")
-                summ += 1
             else:
                 break
         print()
-        return summ
+        return counting
     except:
         return 0
