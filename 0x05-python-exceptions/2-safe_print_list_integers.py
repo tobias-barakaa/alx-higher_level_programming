41eebf44382f231a3801f2359461bcf527cb2b88
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-29 10:50:40 -0700

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,16 +6,11 @@ def safe_print_list_integers(my_list=[], x=0):
     Returns the count of printed integers
     """
     count = 0
-    try:
-        for i in range(x):
-            if type((my_list[i]) == int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-            elif type(x) == int and x > count:
-                raise OverflowError
-            else:
-                break
-    except OverflowError as e:
-        return e
-    print("")
+    for i in range(x):
+       try:
+          print("{:d}".format(my_list[i]), end="")
+          count += 1
+       except ValueError:
+          pass
+    print()
     return count
