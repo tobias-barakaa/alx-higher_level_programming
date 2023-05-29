45e7a69eced43efca5bdc785b25f661c05ade313
tobias-barakaa <tobiasbarakan@gmail.com>
2023-05-29 06:40:50 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,5 @@ def safe_print_list(my_list=[], x=0):
               count += 1
     except indexError:
         break
-    finally:
-        print("")
-        return (count)
+    print("")
+    return (count)
