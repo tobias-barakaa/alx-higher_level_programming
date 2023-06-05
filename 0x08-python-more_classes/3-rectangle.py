e1b00ccef517f53b1975ff74061bc915cc991981
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-05 15:10:15 -0700

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -93,7 +93,7 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join(['#' * self.__width] * self.__height)
+        return '\n'.join(['#' * self.__width] * (self.__height))
 
     def __repr__(self):
         """
