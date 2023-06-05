409f6b69f6b069a68e94aebf48b817ee77dd5d1b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-05 11:15:25 -0700

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+"""0-rectangle, built for Holberton project 0x08 task 0.
+"""
+
+
+class Rectangle:
+    """Empty class per task instructions, will be built upon in later tasks.
+    """
+
+    def __init__(self, _width, _height):
+        """Initializes a Rectangle object with width and height set to 0.
+        """
+        self._width = _width
+        self._height = _height
+
+    @property
+    def width(self):
+        """Getter method to retrieve the width of the rectangle.
+        Returns:
+            The width of the rectangle.
+        """
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """Setter method to set the width of the rectangle.
+        Args:
+            value (int): The width value to be set.
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._width = value
+
+    @property
+    def height(self):
+        """Getter method to retrieve the height of the rectangle.
+        Returns:
+            The height of the rectangle.
+        """
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """Setter method to set the height of the rectangle.
+        Args:
+            value (int): The height value to be set.
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._height = value
