8466297f842845277c6c05d75b40f8d6aa9966b9
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-05 07:37:17 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,81 +1,69 @@
 #!/usr/bin/python3
 
-import sys
+class NQueensSolver:
+    def __init__(self, N):
+        self.N = N
+        self.board = [[0] * N for _ in range(N)]
+        self.solutions = []
 
-def is_safe(board, row, col):
-    # Check if a queen can be placed at the given position without attacking other queens
+    def is_safe(self, row, col):
+        # Check if a queen can be placed at the given position without attacking other queens
 
-    # Check the row on the left side
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+        # Check the row on the left side
+        for i in range(col):
+            if self.board[row][i] == 1:
+                return False
 
-    # Check the upper diagonal on the left side
-    i = row
-    j = col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
+        # Check the upper diagonal on the left side
+        i = row
+        j = col
+        while i >= 0 and j >= 0:
+            if self.board[i][j] == 1:
+                return False
+            i -= 1
+            j -= 1
 
-    # Check the lower diagonal on the left side
-    i = row
-    j = col
-    while i < N and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i += 1
-        j -= 1
+        # Check the lower diagonal on the left side
+        i = row
+        j = col
+        while i < self.N and j >= 0:
+            if self.board[i][j] == 1:
+                return False
+            i += 1
+            j -= 1
 
-    return True
-
-def solve_nqueens(board, col):
-    # Recursive function to solve the N queens problem
-
-    # If all queens are placed, print the solution
-    if col >= N:
-        print_solution(board)
         return True
 
-    # Try placing a queen in each row of the current column
-    for i in range(N):
-        if is_safe(board, i, col):
-            # Place the queen
-            board[i][col] = 1
+    def solve_nqueens(self, col):
+        # Recursive function to solve the N queens problem
 
-            # Recur to place the rest of the queens
-            solve_nqueens(board, col + 1)
+        # If all queens are placed, add the solution to the list
+        if col >= self.N:
+            self.add_solution()
+            return True
 
-            # Backtrack and remove the queen from the current position
-            board[i][col] = 0
+        # Try placing a queen in each row of the current column
+        for i in range(self.N):
+            if self.is_safe(i, col):
+                # Place the queen
+                self.board[i][col] = 1
 
-def print_solution(board):
-    # Print the board configuration
-    solution = []
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] == 1:
-                solution.append([i, j])
-    print(solution)
+                # Recur to place the rest of the queens
+                self.solve_nqueens(col + 1)
 
-# Check the command-line arguments
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+                # Backtrack and remove the queen from the current position
+                self.board[i][col] = 0
 
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    def add_solution(self):
+        # Add the current board configuration as a solution
+        solution = []
+        for i in range(self.N):
+            for j in range(self.N):
+                if self.board[i][j] == 1:
+                    solution.append([i, j])
+        self.solutions.append(solution)
 
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-# Create an empty chessboard
-board = [[0] * N for _ in range(N)]
-
-# Solve the N queens problem
-solve_nqueens(board, 0)
+    def find_solutions(self):
+        # Solve the N queens problem and return the solutions
+        self.solve_nqueens(0)
+        return self.solutions
