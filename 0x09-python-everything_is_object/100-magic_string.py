7baf388629b290333e6c152d2685d1268c9ca7f3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-06 13:45:54 -0700

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+
 def magic_string():
-    count = 0
-    magic_string.n = getattr(magic_string, 'n', count) + 1
+    """
+    Returns a string "BestSchool" repeated n times, where n is the number of times the function has been called.
+    """
+
+    # Retrieve the current value of the counter attribute 'n' from the function object 'magic_string'
+    # If the attribute doesn't exist, initialize it with a value of 0
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+
+    # Return a string consisting of "BestSchool" repeated n-1 times, followed by "BestSchool"
     return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
