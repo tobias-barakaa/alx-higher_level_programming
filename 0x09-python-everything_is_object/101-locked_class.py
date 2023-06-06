e38089a11943337e055aeb8c4648ca678a0bad60
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-06 09:30:58 -0700

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,9 +2,10 @@
 
 """function to define a locked function."""
 
+
 class LockedClass:
     """
-    function that prevents the user from dynamically creating new instance 
+    function that prevents the user from dynamically creating new instance
     attributes, except if the new instance attribute is called first_name.
     """
 
