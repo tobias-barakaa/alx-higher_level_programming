667f395259cf7837bcefc54ac13dea04804a9b6b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-06 11:54:32 -0700

    100-ma

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""define function magc_string"""
+
 
 def magic_string():
+    """
+    functioin that returns a string “BestSchool” n 
+    times the number of the iteration
+    """
     if not hasattr(magic_string, 'count'):
         magic_string.count = 0
     magic_string.count += 1
