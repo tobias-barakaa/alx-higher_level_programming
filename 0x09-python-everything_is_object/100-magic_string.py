688d39335b5043d32693fe940729fa79443ea319
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-06 07:52:10 -0700

    best_sctooh

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def magic_string():
+    if not hasattr(magic_string, 'count'):
+        magic_string.count = 0
+    magic_string.count += 1
+    return "BestSchool" + (", BestSchool" * (magic_string.count - 1))
