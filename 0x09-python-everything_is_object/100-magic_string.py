94cfe6950166b883cda6d83ddcddeb5010479101
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-06 13:24:06 -0700

    magic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-"""
-define function magic_string
-"""
-
-
 def magic_string():
-    """
-    function that returns a string "BestSchool" n 
-    times the number of the iteration
-    """
-
     magic_string.n = getattr(magic_string, 'n', 0) + 1
     return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
