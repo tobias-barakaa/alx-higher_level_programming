5d4137cad30f9c55d54f68cb35f3f6999c1c3bf8
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-12 14:54:23 -0700

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""
+function that returns True if the object is an instance 
+"""
+
+
+class MyList(list):
+    def inherits_from(obj, a_class):
+    """
+    Check if the object is an instance of a class that inherited (directly or indirectly)
+    from the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        True if the object is an instance of a class that inherited from `a_class`,
+        otherwise False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
