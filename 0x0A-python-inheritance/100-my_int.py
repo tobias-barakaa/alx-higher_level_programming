0449f9d1a10b4947646ecc27c1016f83c50bc625
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-06-12 20:15:08 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-'''
-Write a class MyInt that inherits from int
-'''
+
+"""function that inherits from int"""
 
 
 class MyInt(int):
+    def __eq__(self, other):
+        """
+        Overrides the == operator to invert its behavior.
+        """
+        return int(self) == int(other)
 
-    def __eq__(self, value):
-        return int(self) != int(value)
-
-    def __ne__(self, value):
-        return int(self) == int(value)
+    def __ne__(self, other):
+        """
+        Overrides the != operator to invert its behavior.
+        """
+        return int(self) != int(other)
