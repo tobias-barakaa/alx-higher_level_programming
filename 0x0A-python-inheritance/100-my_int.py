2eb53b5712bc96b2c2299d703fc78180596f9ca3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-12 09:59:51 -0700

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""function that inherits from int"""
+
+class MyInt(int):
+    def __eq__(self, other):
+        """
+        Overrides the == operator to invert its behavior.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Overrides the != operator to invert its behavior.
+        """
+        return super().__eq__(other)
