38fa7f0438e23e111ed4db1b5e8055cfc6c08b5b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-12 15:54:30 -0700

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pythone3
+
+"""function"""
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Calculates the area of the geometry.
+
+        Raises:
+            Exception: If the area() method is not implemented in the subclass.
+        """
+        raise Exception("area() is not implemented")
