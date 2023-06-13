f99a145dbc99b61fbf59d36772b2cf19a892083d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-13 16:56:47 -0700

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""function that that defines a student by: (based on 9-student.py)"""
+"""Student class defines a student"""
 
 
 class Student:
-    """function that that defines a student by: (based on 9-student.py)"""
+    """Defines a student"""
+
     def __init__(self, first_name, last_name, age):
+        """Instantiation of a Student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs is None:
-            return class_to_json(self)
-        else:
-            description = {}
-            for attr in attrs:
-                if hasattr(self, attr):
-                    value = getattr(self, attr)
-                    if isinstance(value, (list, dict, str, int, bool)):
-                        description[attr] = value
-            return description
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list of str): List of attribute names. If provided,
+                only attributes contained in this list will be retrieved.
+                Otherwise, all attributes will be retrieved.
+
+        Returns:
+            dict: Dictionary representation of the Student instance.
+
+        """
+        if isinstance(attrs, list) and all(isinstance(elem, str) for elem in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
