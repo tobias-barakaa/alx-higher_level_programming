335bbcee442335a33f1d40ff9426831f4c1f9c22
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-13 10:09:36 -0700

    0-

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""function to read from file"""
+
+def read_file(filename=""):
+    with open(filename, "r", encoding="utf8") as text_file:
+        print(text_file.read(), end="")
