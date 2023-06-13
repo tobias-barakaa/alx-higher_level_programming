bedb5a20bc6e0c52fdd2b7227ac43e5c6a5cc21a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-13 14:25:05 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,4 +6,4 @@
 def append_write(filename="", text=""):
     """function to write text to a file"""
     with open(filename, "a", encoding="UTF-8") as file:
-         return file.write(text)      
+        return file.write(text)
