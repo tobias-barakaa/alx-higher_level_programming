ec24175d520a542de17ad0fc13ce55a114934740
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-13 14:14:22 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,9 +5,5 @@
 
 def write_file(filename="", text=""):
     """function to write text to a file"""
-    count = 0  # Initialize count to 0
     with open(filename, "a", encoding="UTF-8") as file:
         file.write(text)
-        count = len(text)  # Count the number of characters in the text
-
-    return count
