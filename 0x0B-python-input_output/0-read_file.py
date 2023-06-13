fdc175678be3b6dc5eb358cea845e72fbd4e78ad
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-13 10:13:51 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 """function to read from file"""
 
 def read_file(filename=""):
+    """read file and print in stdout"""
     with open(filename, "r", encoding="utf8") as text_file:
         print(text_file.read(), end="")
