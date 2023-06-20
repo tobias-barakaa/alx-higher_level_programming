a250a19997195644b88d9a1e62672e9bf67cf374
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-19 19:23:22 -0700

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,3 +30,53 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    @staticmethod
+    def to_json_string(list_dictionaries):
+    """
+    Return the JSON string representation of a list of dictionaries.
+
+    Args:
+        list_dictionaries (list): List of dictionaries.
+
+    Returns:
+        str: JSON string representation of list_dictionaries.
+    """
+    if list_dictionaries is None or len(list_dictionaries) == 0:
+        return "[]"
+    return json.dumps(list_dictionaries)
+
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+    """
+    Write the JSON string representation of list_objs to a file.
+
+    Args:
+        cls (class): Class.
+        list_objs (list): List of instances that inherit from Base.
+    """
+        filename = "{:s}.json".format(cls.__name__)
+        content = []
+
+        if list_objs is not None:
+            for obj in list_objs:
+                content.append(cls.to_dictionary(obj))
+
+        with open(filename, mode="w", encoding="utf-8") as file:
+            file.write(cls.to_json_string(content))
+
+
+    @staticmethod
+    def from_json_string(json_string):
+    """
+    Return a list of dictionaries from the JSON string representation.
+
+    Args:
+        json_string (str): String representing a list of dictionaries.
+
+    Returns:
+        list: List of dictionaries.
+    """
+        if json_string is None or json_string == "":
+            return []
+         return json.loads(json_string)           
