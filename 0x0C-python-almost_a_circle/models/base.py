6e9d0ceaeae9e3286e140f673078e33e88c0ff03
tobias-barakaa <tobiasbarakan@gmail.com>
2023-06-19 18:06:39 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""Defines a base model class."""
-
 
 class Base:
-    """Represent the base model.
-
-    Represents the "base" for all other classes in project 0x0C*.
+    """
+    Base class for creating objects with an id attribute.
 
     Attributes:
-        __nb_objects (int): The number of instantiated Bases.
+        __nb_objects (int): Counter for generating unique ids.
+
+    Methods:
+        __init__(self, id=None): Initializes a Base object with a unique id.
     """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base.
+        """
+        Initializes a Base object with a unique id.
 
         Args:
-            id (int): The identity of the new Base.
+            id (int): Optional id value. If provided, the object is assigned the given id.
+                      If not provided, a unique id is generated for the object.
         """
         if id is not None:
             self.id = id
