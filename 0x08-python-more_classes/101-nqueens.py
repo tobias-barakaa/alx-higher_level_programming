f9c36dfa642bc48b939c41eeedf22fe50062a51f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-01 03:20:10 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,69 +1,15 @@
 #!/usr/bin/python3
+"""defines program to solve the N queens problem"""
 
-class NQueensSolver:
-    def __init__(self, N):
-        self.N = N
-        self.board = [[0] * N for _ in range(N)]
-        self.solutions = []
 
-    def is_safe(self, row, col):
-        # Check if a queen can be placed at the given position without attacking other queens
-
-        # Check the row on the left side
-        for i in range(col):
-            if self.board[row][i] == 1:
-                return False
-
-        # Check the upper diagonal on the left side
-        i = row
-        j = col
-        while i >= 0 and j >= 0:
-            if self.board[i][j] == 1:
-                return False
-            i -= 1
-            j -= 1
-
-        # Check the lower diagonal on the left side
-        i = row
-        j = col
-        while i < self.N and j >= 0:
-            if self.board[i][j] == 1:
-                return False
-            i += 1
-            j -= 1
-
-        return True
-
-    def solve_nqueens(self, col):
-        # Recursive function to solve the N queens problem
-
-        # If all queens are placed, add the solution to the list
-        if col >= self.N:
-            self.add_solution()
-            return True
-
-        # Try placing a queen in each row of the current column
-        for i in range(self.N):
-            if self.is_safe(i, col):
-                # Place the queen
-                self.board[i][col] = 1
-
-                # Recur to place the rest of the queens
-                self.solve_nqueens(col + 1)
-
-                # Backtrack and remove the queen from the current position
-                self.board[i][col] = 0
-
-    def add_solution(self):
-        # Add the current board configuration as a solution
-        solution = []
-        for i in range(self.N):
-            for j in range(self.N):
-                if self.board[i][j] == 1:
-                    solution.append([i, j])
-        self.solutions.append(solution)
-
-    def find_solutions(self):
-        # Solve the N queens problem and return the solutions
-        self.solve_nqueens(0)
-        return self.solutions
+import sys
+if len(sys.args) is not 2:
+    print("Usage: nqueens N")
+    exit(1)
+N = sys.args[1]
+if type(N) is not int:
+    print("N must be a number")
+    exit(1)
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
