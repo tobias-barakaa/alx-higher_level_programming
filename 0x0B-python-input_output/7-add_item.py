baf25021502d5edda95de6b4940183eae369a86f
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-01 00:40:01 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@
 from sys import argv
 from os.path import exists
 
-tofile_json = __import__("5-save_to_json_file").tofile_json
-jso_loading = __import__("6-load_from_json_file").jso_loading
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 namefile = "add_item.json"
 argc = len(argv)
@@ -14,11 +14,11 @@ argc = len(argv)
 new_list = []
 
 if exists(namefile):
-    new_list = jso_loading(namefile)
+    new_list = load_from_json_file(namefile)
 
 if (argc == 1):
-    tofile_json([], namefile)
+    save_to_json_file([], namefile)
 else:
     for i in range(1, argc):
         new_list.append(argv[i])
-    tofile_json(new_list, namefile)
+    save_to_json_file(new_list, namefile)
