92644fab6926f62baa75ebd6b9cb5250bbdda6ad
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-20 12:23:40 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,11 @@
--- Creates the database hbtn_0d_2 if it doesn't exist
-  2 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-  3 
-  4 -- Create the user user_0d_2 if it doesn't exist and set the password
-  5 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-  6 
-  7 -- Grant SELECT privilege on hbtn_0d_2 to the user user_0d_2
-  8 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-  9 
- 10 -- Refresh the privileges
- 11 FLUSH PRIVILEGES;
+-- write a script to Creates the database 
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_2;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON hbtn_0d_2.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
