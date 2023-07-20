180bbf0900e62f0850d085a5b4523c99adfbf00a
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-20 12:44:12 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,15 +1,12 @@
-# First, create and grant privileges to 'user_0d_1'
-echo "CREATE USER 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-
-# Next, create and grant privileges to 'user_0d_2'
-echo "CREATE USER 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
-echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
-
-# Now, list the privileges for 'user_0d_1'
-echo "Grants for user_0d_1@localhost"
-echo "SHOW GRANTS FOR 'user_0d_1'@'localhost';" | mysql -hlocalhost -uroot -p
-
-# Finally, list the privileges for 'user_0d_2'
-echo "Grants for user_0d_2@localhost"
-echo "SHOW GRANTS FOR 'user_0d_2'@'localhost';" | mysql -hlocalhost -uroot -p
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
