77a8effc1a83b0b6c33d4976d3fd34833fa38041
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-20 12:06:30 -0700

    2-create_read_user.sql

--- 2-create_read_user.sql
+++ 2-create_read_user.sql
@@ -1,4 +1,12 @@
--- Create the database 
+-- Creates the database hbtn_0d_2 if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user user_0d_2 if it doesn't exist and set the password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on hbtn_0d_2 to the user user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Refresh the privileges
+FLUSH PRIVILEGES;
+
