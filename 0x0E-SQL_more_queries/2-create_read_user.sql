7b0b33975471d5da5ab3a9f3120edc2784f4d93d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-07-20 12:29:24 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,11 @@
 -- write a script to Creates the database 
 CREATE DATABASE
-    IF NOT EXISTS hbtn_0d_2;
+    IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-   ON hbtn_0d_2.*
+   ON `hbtn_0d_2`.*
    TO 'user_0d_2'@'localhost'
    IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
