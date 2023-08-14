cd5f46e78bc1bf285675cb6b873d4ee335f2d22b
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-14 10:03:38 -0700

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const arguments = process.argv.slice(2)[0]; 
+
+if (!isNaN(arguments)) {
+    console.log('My number:', arguments);
+} else {
+    const isNumber = Number(arguments);
+    if (!isNaN(isNumber)) {
+        console.log("My number:", isNumber);
+    } else {
+        console.log("Not a number");
+    }
+}
