1ed0adf446a3cbfb5341808e46c6dc2030441c49
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-14 22:15:37 +0300

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
-const args = parseInt(process.argv[2]);
+const args = Number(process.argv[2]);
 if (args)
+{
   console.log(`My number: ${args}`);
+}
 } else
+{
   console.log('Not a number');
+}
