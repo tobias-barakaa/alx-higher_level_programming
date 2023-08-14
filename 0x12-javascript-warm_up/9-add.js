f5e22c9d18775f4920a44bde70b83994351fb6a3
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-14 16:27:10 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
 function add(a, b) {
-    return a + b;
-}
+     element = a + b;
+     return element;
+ }
 
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-
-if ((arg1) && (arg2)) {
-    const result = add(arg1, arg2);
-    console.log(result);
-} else {
-    console.log('NaN');
-}
+console.log(add(a,b));
