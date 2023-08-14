34fc87e444d6d428575f7feed5943d8e3504f056
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-15 02:50:13 +0300

    Update 101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-        theFunction();
+exports.callMeMoby = function (x, theFunc) {
+  for (let i = 0; i < x; i++) {
+    theFunc();
   }
 };
