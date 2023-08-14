481ed8b2f7792ff3d8a0f1352d634e4dcd340f63
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-14 06:59:05 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const arguments = process.argv.slice(2);
-
-if (arguments.length === 0) {
+const arguments = process.argv;
+if (arguments.length === 2) {
   console.log('No arguments');
-} else if (arguments.length === 1) {
+} else if (arguments.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
