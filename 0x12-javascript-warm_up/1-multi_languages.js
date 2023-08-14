def1a8c70bc0e09570b7ac700e59215660b7af9c
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-14 06:22:52 -0700

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myVar = 'C is fun';
+console.log(myVar);
+const anotherVar = 'Python is cool'
+console.log(anotherVar);
+const lastVar = 'JavaScript is amazing'
+console.log(lastVar);
