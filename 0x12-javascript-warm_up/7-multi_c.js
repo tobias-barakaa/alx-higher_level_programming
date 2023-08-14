e44d3d890edc773a1a4d0f15347aa5eccf872c84
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-15 01:22:29 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const args = Number(process.argv[2]);
-if (args)
-{
-     for (let i = 0; i < args; i++) {
-          console.log('C is fun');
-     }
-} else if (isNaN(args)) {
-     console.log('Missing number of occurrences');
+
+if (args) {
+  for (let i = 0; i < args; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
