75f58f99f0d48126e2bcd2ddf649730290dadad8
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-14 08:27:37 -0700

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-if (!process.argv[2]) {
+const arg = process.argv[2];
+if (!arg) {
     console.log("No argument");
 } else {
-    console.log(process.argv[2]);
+    console.log(arg);
 }
