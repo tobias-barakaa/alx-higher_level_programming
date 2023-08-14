813d456a2ee32d0ed2c48b4906bb9269749e3921
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-15 02:36:01 +0300

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,20 +1,15 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    if (isNaN(n)) {
-        return 1;
-    }
+const n = parseInt(process.argv[2]);
 
-    n = parseInt(n);
-
-    if (n <= 1) {
-        return 1;
-    }
-
-    return n * factorial(n - 1);
+function factorial (n) {
+  if (Number.isNaN(n) || n === 0) {
+    return (1);
+  }
+  if (n < 0) {
+    return (-1);
+  }
+  return (n * factorial(n - 1));
 }
-
-const arg = process.argv[2];
-const result = factorial(arg);
-
-console.log(result);
+const fact = factorial(n)
+console.log(fact);
