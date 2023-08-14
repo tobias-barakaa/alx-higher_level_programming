0ee9cc6fce11e15d8c0086beb68453160aeaed62
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-14 13:15:39 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 const arrVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for(i = 0; i < arrVar.length; i++)
-console.log(arrVar[i]);
+for(const arr of arrVar)
+console.log(arr);
