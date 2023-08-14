8f255511919562fe26c36516ca1b41ce03ce43d7
tobias-barakaa <tobiasbarakan@gmail.com>
2023-08-14 14:44:13 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,10 +3,10 @@
 const args = process.argv.slice(2);
 const x = parseInt(args[0]);
 
-if (isNaN(x) || x <= 0) {
-    console.log("Missing number of occurrences");
+if(args) {
+     for (let i = 0; i < args.length; i++) {
+     console.log("C is fun");
+     } 
 } else {
-    for (let i = 0; i < x; i++) {
-        console.log("C is fun");
-    }
+     console.log("Missing number of occurrences");
 }
