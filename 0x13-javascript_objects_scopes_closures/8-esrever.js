59894e353b9e796fbff9d99a5842350a7f654a87
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-16 00:43:19 +0300

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,6 @@
-anything
+#!/usr/bin/node
+// function that returns the number of occurrences in a list
+exports.esrever = function (list) {
+  for (let i = 0; i < list.length; i++)
+      return list[i - 1];    
+}
