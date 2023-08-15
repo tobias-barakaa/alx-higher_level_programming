e74964ed6fc689bb6357494fbe0db4c88976737d
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-15 19:45:18 +0300

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,13 @@
-anything
+#!/usr/bin/node
+// empty class object Rectangle and the constructor;
+class Rectangle {
+  constructor (w, h) {
+    if ((w <= 0) || (h <= 0)) {
+    return {};
+  }
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
