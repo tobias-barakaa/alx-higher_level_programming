846417a2aa0642a3c476bf86ad5db5ea5db7d178
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-16 01:03:51 +0300

    Update 9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // function that returns the number of occurrences in a list
 let count = 0;
-
 exports.logMe = function (item) {
   console.log(`${count}: ${item}`);
   count++;
 };
-
