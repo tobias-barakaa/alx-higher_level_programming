d2d7085fafcc33fd1beef57062198391607d9249
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-16 01:07:18 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+// function that returns the number of occurrences in a list
 const data = require('./100-data');
 const initialList = data.list;
 
@@ -5,4 +7,3 @@ const newList = initialList.map((value, index) => value * index);
 
 console.log(initialList);
 console.log(newList);
-
