9ac0d7798be651c6eaa3ab596fdcbfb2af1cb196
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-15 23:30:04 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-// class object Rectangle and the constructor with width and height;
+// create class `Rectangle` w/ input filter, print instance method
 class Rectangle {
   constructor (w, h) {
-    if (w >= 0 && h >= 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-  
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
