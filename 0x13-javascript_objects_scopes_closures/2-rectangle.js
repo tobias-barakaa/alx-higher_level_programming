b1efa284870001bdd859b8169fb6d8f7ab403d6f
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-15 19:47:37 +0300

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,11 +3,10 @@
 class Rectangle {
   constructor (w, h) {
     if ((w <= 0) || (h <= 0)) {
-    return {};
-  }
     this.width = w;
     this.height = h;
-  }
+   }
+ }
 }
 
 module.exports = Rectangle;
