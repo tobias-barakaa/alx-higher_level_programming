0799cba6dbea0c0dca5d75ef914266a63ddc4703
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-16 00:23:45 +0300

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,17 @@
-anything
+#!/usr/bin/node
+// class object Rectangle and the constructor with width and height;
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
