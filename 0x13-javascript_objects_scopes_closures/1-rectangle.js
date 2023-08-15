d2ccbf382b1e31281baee0b36dd3755d3026b0e9
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-15 18:39:14 +0300

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,1 +1,11 @@
-anything
+#!/usr/bin/node
+// empty class object(Rectangle) and the constructor;
+
+class Rectangle {
+  constructor() {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
