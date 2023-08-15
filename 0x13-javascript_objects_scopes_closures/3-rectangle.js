cda2f5d3d404e1c0b36136a534c7e89288fe031d
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-15 23:11:29 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,7 +10,7 @@ class Rectangle {
   
   print () {
     for (let i = 0; i < this.height; i++) {
-      let row = "";
+      let row = '';
       for (let j = 0; j < this.width; j++) {
         row += 'X';
       }
