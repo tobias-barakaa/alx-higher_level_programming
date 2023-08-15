bfe91341834948adf7c700d21d6119ceb75cd374
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-15 22:34:57 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,21 @@
-anything
+#!/usr/bin/node
+// class object Rectangle and the constructor with width and height;
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 0 && h >= 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.width; i++) {
+    let row = ""
+      for (let j = 0; j < this.height; j++) {
+      row += "X";
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
