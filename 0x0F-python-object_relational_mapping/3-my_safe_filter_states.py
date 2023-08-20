fd9b73cddc63624d8ebf56f7ce52e47a62217250
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-21 02:20:33 +0300

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -40,7 +40,8 @@ def main():
     cursor = connection.cursor()
 
     # Execute the SQL query to select all states.
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name = %s\
+            ORDER BY states.id ASC", query)
 
     # Iterate over the results and print each row.
     rows = cursor.fetchall()
