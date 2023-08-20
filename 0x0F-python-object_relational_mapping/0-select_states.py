ceb64bbda1710ad4fa1f4dc32b62079058f8f23b
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-20 21:06:24 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,45 +1,32 @@
 #!/usr/bin/python3
 """
-Script lists all states from database hbtn_0e_0_usa
-Takes three arguments:
-    mysql username
-    mysql password
-    database name
-Connects to default host (localhost) and port (3306)
+This script lists all states from the hbtn_0e_0_usa database.
+It takes three command-line arguments:
+    - MySQL username
+    - MySQL password
+    - Database name
+The script connects to the MySQL server on the default host (localhost) and port (3306).
 """
 
-import sys
-import MySQLdb
-
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    mysql_username, mysql_password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    try:
-        db = MySQLdb.connect(
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
-        )
-
-        cursor = db.cursor()
-
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-
-        cursor.close()
-        db.close()
-        
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-
 if __name__ == "__main__":
-    main()
+    from sys import argv
+    import MySQLdb
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
