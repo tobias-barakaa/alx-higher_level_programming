ba51b4049bf1e54d126c59a9069e5fc5902bde91
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-20 20:41:48 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,18 @@
-nything
-`:Wq
+#!/usr/bin/env python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+  db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+  d = db.cursor()
+  d.execute("SELECT * FROM states ORDER BY id ASC")
+  fetched = d.fetchall()
+  for rows in fetched:
+    print(rows)
+  cursor.close()
+  db.close()
 
