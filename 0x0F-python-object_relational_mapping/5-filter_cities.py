99e0457993f5b17fa67fdfe73f076e117a3f2e6b
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-21 04:14:22 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,43 +1,30 @@
 #!/usr/bin/python3
 
 """
-This script lists all `cities` from the database `hbtn_0e_4_usa`.
+This script lists all `cities` in the `cities` table of the `hbtn_0e_4_usa` database
+where the city's state matches the argument `state name`.
 
-The script takes three arguments:
+The script takes four arguments:
     * The MySQL username
     * The MySQL password
     * The database name
+    * The state name to search for
 
-The script connects to the MySQL database on the default
-host (localhost) and port (3306).
+The script connects to the MySQL database on the default host (localhost) and port (3306).
 
-The script uses the `sys` module to get the MySQL username,
-password, and database name from the command line arguments.
+The script uses the `sys` module to get the MySQL username, password, and database name from the command line arguments.
 
-The script uses the `MySQLdb` module to execute the SQL query
-`SELECT c.id, c.name, s.name FROM cities c INNER JOIN states s
-ON c.state_id = s.id ORDER BY c.id`.
+The script uses the `MySQLdb` module to execute the SQL query `SELECT c.name FROM cities c INNER JOIN states s ON c.state_id = s.id WHERE s.name = %s ORDER BY c.id`.
 
-The results of the query are then printed to the console.
-
-Here is a breakdown of the SQL query:
-
-* `SELECT c.id, c.name, s.name`: This selects the `id`, `name`,
-and `name` columns from the `cities` and `states` tables.
-* `FROM cities c INNER JOIN states s`: This joins the `cities`
-and `states` tables on the `state_id` column.
-* `ON c.state_id = s.id`: This specifies the join condition.
-* `ORDER BY c.id`: This sorts the results by the `id` column.
+The results of the query are then printed to the console, separated by commas.
 """
 
 import sys
 import MySQLdb
 
-
 def main():
     """
-    This function connects to the MySQL database and
-    lists all cities from the cities table.
+    This function connects to the MySQL database and lists all cities from the cities table where the city's state matches the argument.
     """
 
     # Get the MySQL username from the command line arguments.
@@ -49,30 +36,30 @@ def main():
     # Get the database name from the command line arguments.
     database = sys.argv[3]
 
+    # Get the state name to search for from the command line arguments.
+    state_name = sys.argv[4]
+
     # Connect to the MySQL database.
     connection = MySQLdb.connect(user=username, passwd=password, db=database)
 
     # Create a cursor object.
     cursor = connection.cursor()
-    state = sys.argv[4]
 
     # Execute the SQL query.
-    query = ("SELECT c.name \
-                 FROM cities c INNER JOIN states s \
-                 ON c.state_id = s.id WHERE s.name = %s\
-                 ORDER BY c.id", (state, ))
+    query = "SELECT c.name FROM cities c INNER JOIN states s ON c.state_id = s.id WHERE s.name = %s ORDER BY c.id"
+    cursor.execute(query, (state_name,))
 
     # Iterate over the results and print each row.
     rows = cursor.fetchall()
     for row in rows:
-        print(row)
+        print(row[0], end=", ")
+    print("")
 
     # Close the cursor object.
     cursor.close()
 
     # Close the connection to the database.
     connection.close()
-
 
 if __name__ == "__main__":
     main()
