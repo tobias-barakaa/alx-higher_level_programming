43c4f6123e8f4755d293b9eaef0a42373e073436
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-08-21 04:15:48 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,65 +1,44 @@
 #!/usr/bin/python3
-
 """
-This script lists all `cities` in the `cities` table of the `hbtn_0e_4_usa` database
+Script that lists all `cities` in the `cities` table of `hbtn_0e_4_usa`
 where the city's state matches the argument `state name`.
 
-The script takes four arguments:
-    * The MySQL username
-    * The MySQL password
-    * The database name
-    * The state name to search for
-
-The script connects to the MySQL database on the default host (localhost) and port (3306).
-
-The script uses the `sys` module to get the MySQL username, password, and database name from the command line arguments.
-
-The script uses the `MySQLdb` module to execute the SQL query `SELECT c.name FROM cities c INNER JOIN states s ON c.state_id = s.id WHERE s.name = %s ORDER BY c.id`.
-
-The results of the query are then printed to the console, separated by commas.
+Arguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+    state name (str)
 """
 
 import sys
 import MySQLdb
 
-def main():
-    """
-    This function connects to the MySQL database and lists all cities from the cities table where the city's state matches the argument.
-    """
-
-    # Get the MySQL username from the command line arguments.
-    username = sys.argv[1]
-
-    # Get the MySQL password from the command line arguments.
-    password = sys.argv[2]
-
-    # Get the database name from the command line arguments.
-    database = sys.argv[3]
-
-    # Get the state name to search for from the command line arguments.
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL database.
-    connection = MySQLdb.connect(user=username, passwd=password, db=database)
-
-    # Create a cursor object.
-    cursor = connection.cursor()
-
-    # Execute the SQL query.
-    query = "SELECT c.name FROM cities c INNER JOIN states s ON c.state_id = s.id WHERE s.name = %s ORDER BY c.id"
-    cursor.execute(query, (state_name,))
-
-    # Iterate over the results and print each row.
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row[0], end=", ")
-    print("")
-
-    # Close the cursor object.
-    cursor.close()
-
-    # Close the connection to the database.
-    connection.close()
-
 if __name__ == "__main__":
-    main()
+    # Check if all four arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <mysql username> <mysql password> <database name> <state name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(user=username, passwd=password, db=database)
+    cursor = db.cursor()
+
+    # Create and execute the query to fetch cities for the given state
+    query = "SELECT name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = %s) ORDER BY id"
+    cursor.execute(query, (state,))
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Display the results
+    cities = [row[0] for row in results]
+    print(", ".join(cities))
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
