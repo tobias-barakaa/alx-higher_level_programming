0ac7413f1c20cc7c8df315b5de9a5ce3a44b7dbe
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-01 00:56:15 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Get the URL from the user
-url=$1
-
-# Use curl to get the response body size
-body_size=$(curl -s -I "$url" | grep -Po 'Content-Length:\s*([0-9]+)')
-
-# Print the body size
-echo "Body size: ${body_size} bytes"
+# a Bash script that takes in a URL, sends a request to that URL
+curl -s "$1" | wc -c
