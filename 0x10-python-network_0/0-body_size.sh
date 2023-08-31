e04b4b4425765a62e0ccaf5b97d3480e28900dcb
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-01 00:40:22 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-# Check if the URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-URL="$1"
-
-# Send a request to the URL using curl, follow redirects, and store the response in a temporary file
-temp_file=$(mktemp)
-curl -sL "$URL" -o "$temp_file"
-
-# Get the size of the response body in bytes and display it
-body_size=$(wc -c < "$temp_file")
-echo "$body_size"
-
-# Clean up by removing the temporary file
-rm "$temp_file"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
