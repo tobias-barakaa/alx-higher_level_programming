7524dbe40a98b23ab541fa423aac653c12cc7eea
tobias-barakaa <tobiasbarakan@gmail.com>
2023-09-01 12:11:36 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Python script that fetches"""
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+try:
+    with urllib.request.urlopen(url) as response:
+        # Read the response body
+        response_data = response.read()
+
+        # Display the response information
+        print("Body response:")
+        print(f"    - type: {type(response_data)}")
+        print(f"    - content: {response_data}")
+        print(f"    - utf8 content: {response_data.decode('utf-8')}")
+
+except urllib.error.URLError as e:
+    print(f"Error: {e}")
+
