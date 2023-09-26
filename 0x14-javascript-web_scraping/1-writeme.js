663a3fe6bc553c583856645bda1f036c7a1e63b3
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 00:56:11 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
-fs.writeFile(filePath, content, 'utf-8', (err,data) => {
+fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.log(err);
   } 
