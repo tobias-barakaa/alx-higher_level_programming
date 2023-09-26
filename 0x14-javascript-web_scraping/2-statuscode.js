ab9e216d704c8e4fa6dd5142c7294c02c8d740b7
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 01:21:34 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
-any
+#!/usr/bin/node
+
+const request = require("request");
+const url = process.argv[2];
+
+request(url, (err,data) => {
+  if(err)
+        console.log(error.message);
+  else 
+        console.log(`code: ${data.statusCode}`)
