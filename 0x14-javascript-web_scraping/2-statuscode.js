20ae94c0c131681ab7aff09979b56be48be0f2c4
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 02:12:46 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-// Import the 'request' module for making HTTP requests
 const request = require('request');
-// Get the URL to request from the first command line argument
 const url = process.argv[2];
-// Make a GET request to the specified URL
 request.get(url, (err, response) => {
   if (err) {
-    // If there's an error during the request, log the error
-    console.log(err);
+    // Log an informative error message
+    console.log(err.message);
   } else {
-    // If the request is successful, log the HTTP status code from the response
     console.log("code:", response.statusCode);
   }
 });
