8291afc6d70569e79bea43110af2d3c73765d243
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 00:07:17 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,12 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+// Check if the correct number of command line arguments is provided
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
     process.stdout.write(data);
   }
