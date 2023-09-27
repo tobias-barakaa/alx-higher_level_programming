8056f818f70d88242dacfeaa0a284a11a28d01ec
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 22:00:05 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,30 +6,37 @@ const request = require('request');
 // Get the API URL from the command line arguments
 const apiUrl = process.argv[2];
 
+// Check if the API URL is provided
+if (!apiUrl) {
+  console.error('Error: Please provide an API URL as the first command line argument.');
+  process.exit(1);
+}
+
 // Send an HTTP GET request to the provided API URL
-request(apiUrl, (error, response, body) => {
-   // Parse the JSON response from the API
-   const filmsData = JSON.parse(body);
+request(apiUrl, async (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
 
-   // Initialize a count variable to keep track of the number of characters
-   let count = 0;
+  // Parse the JSON response from the API
+  const filmsData = JSON.parse(body);
 
-   // Loop through the films in the API response
-   for (let i = 0; i < filmsData.results.length; i++) {
-      const film = filmsData.results[i];
+  // Initialize a count variable to keep track of the number of characters
+  let count = 0;
 
-      // Loop through the characters in the current film
-      for (let j = 0; j < film.characters.length; j++) {
-        const characterUrl = film.characters[j];
-
-        // Check if the character URL contains '18' (character ID 18)
-        if (characterUrl.includes('18')) {
-           // Increment the count if the character ID is 18
-           count++;
-        }
+  // Loop through the films in the API response
+  for (const film of filmsData.results) {
+    // Loop through the characters in the current film
+    for (const characterUrl of film.characters) {
+      // Check if the character URL contains '18' (character ID 18)
+      if (characterUrl.includes('18')) {
+        // Increment the count if the character ID is 18
+        count++;
       }
     }
+  }
 
-    // Print the total count of characters with ID 18
-    console.log(count);
+  // Print the total count of characters with ID 18
+  console.log(count);
 });
