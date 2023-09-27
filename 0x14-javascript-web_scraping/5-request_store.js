6f653f86a009deb3b2e8a43516b5585c3ac66ba9
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 22:19:23 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,32 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-  console.log('Usage: node 5-request_store.js <URL> <outputFilePath>');
-  process.exit(1);
-}
-
 const url = process.argv[2];
 const outputPath = process.argv[3];
 
 request(url, (error, response, body) => {
   if (error) {
-    console.log('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.log('HTTP Request Failed with Status Code:', response.statusCode);
+    console.log(error);
     process.exit(1);
   }
 
   // Write the response body to the specified file (UTF-8 encoded)
   fs.writeFile(outputPath, body, 'utf-8', (error) => {
     if (error) {
-      console.log('Error writing to file:', writeError);
+      console.log(error);
       process.exit(1);
     }
-    console.log('File saved successfully:', outputPath);
   });
 });
