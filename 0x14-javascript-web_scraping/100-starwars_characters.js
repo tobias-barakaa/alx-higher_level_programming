db7dda42dbb625781ce5196b6a01b495ed30e345
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-28 02:29:00 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,47 +1,54 @@
 #!/usr/bin/node
-// A script that prints the number of movies where the character “Wedge Antilles” is present
 
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
+if (process.argv.length !== 3) {
+  console.error('Usage: node 100-starwars_characters.js <Movie_ID>');
+  process.exit(1);
+}
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-let filmChars = [];
-const charNames = {};
-
-request(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
-  } else {
-    filmChars = body.characters;
-    const requests = filmChars.map((index) => {
-      return new Promise((resolve, reject) => {
-        request({ url: index, json: true }, (err, res, body) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(body.name);
-          }
-        });
-      });
-    });
-
-    Promise.all(requests)
-      .then((names) => {
-        names.forEach((name, index) => {
-          getName(filmChars[index], name);
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    process.exit(1);
   }
+
+  if (response.statusCode !== 200) {
+    console.log(response.statusCode);
+    process.exit(1);
+  }
+
+  const movieData = JSON.parse(body);
+
+  if (!movieData.characters || movieData.characters.length === 0) {
+    console.log('No characters found for this movie.');
+    process.exit(0);
+  }
+
+  // Fetch and print character names
+  fetchAndPrintCharacterNames(movieData.characters);
 });
 
-// function that prints the name of each character
-function getName(url, name) {
-  charNames[url] = name;
-  if (Object.keys(charNames).length === filmChars.length) {
-    filmChars.forEach((idx) => {
-      console.log(charNames[idx]);
+function fetchAndPrintCharacterNames(characterUrls) {
+  const characters = [];
+
+  // Function to fetch character names from their URLs
+  function fetchCharacterName(url) {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const characterData = JSON.parse(body);
+        characters.push(characterData.name);
+        // If all characters are fetched, print them
+        if (characters.length === characterUrls.length) {
+          characters.forEach((character) => console.log(character));
+        }
+      } else {
+        console.log(error);
+      }
     });
   }
+
+  // Fetch names for all character URLs
+  characterUrls.forEach((url) => fetchCharacterName(url));
 }
