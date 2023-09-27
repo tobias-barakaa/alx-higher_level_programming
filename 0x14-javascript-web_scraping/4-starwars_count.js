30470d857981b289e4c1e9245c24caeb602e675b
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 14:47:10 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -25,7 +25,7 @@ request(apiUrl, (error, response, body) => {
       // Loop through the films and count the occurrences of Wedge Antilles
       for (const film of filmsData) {
         if (film.characters.includes(`https://swapi-api.alx-tools.com/api/films/${characterId}`)) {
-          count++;
+          count += 1;
         }
       }
 
