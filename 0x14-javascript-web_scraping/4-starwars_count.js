d648fa378dc8d87933439bc1d8c64a99639bb4f5
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 19:30:45 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,27 +4,19 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.log(error)
-  }
-  try {
+try {
     const filmsData = JSON.parse(body);
-
     // Initialize a count variable
     let count = 0;
-
     // Loop through the films
     for (let i = 0; i < filmsData.results.length; i++) {
       const film = filmsData.results[i];
-      
       // Loop through the characters in the current film
       for (let j = 0; j < film.characters.length; j++) {
         const characterUrl = film.characters[j];
-        
         // Check if the character URL contains '/18/' to match character ID 18
-        if (characterUrl.includes('/18/')) {
+        if (characterUrl.search('/18/')) {
            count++;
-          // Exit the inner loop when a match is found for this film
         }
       }
     }
