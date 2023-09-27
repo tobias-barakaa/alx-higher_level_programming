550149f4da0238085e488b09ba0f2b91bac76f75
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-28 01:14:34 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // A script that prints all characters of a Star Wars movie
-
 const request = require('request');
 const movieId = process.argv[2];
 
@@ -8,16 +7,15 @@ const baseUrl = 'https://swapi.dev/api/films/';
 
 request(baseUrl + movieId, { json: true }, (err, res, filmData) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     const characterUrls = filmData.characters;
-
     // Function to fetch character names and print them
     function fetchAndPrintCharacterNames(urls, index = 0) {
       if (index < urls.length) {
         request(urls[index], { json: true }, (err, res, characterData) => {
           if (err) {
-            console.error(err);
+            console.log(err);
           } else {
             console.log(characterData.name);
             fetchAndPrintCharacterNames(urls, index + 1);
