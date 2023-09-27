6ab5053d89c5ebac70b552feb73492d336fc8043
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-28 01:10:31 +0300

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,45 +1,31 @@
 #!/usr/bin/node
+// A script that prints all characters of a Star Wars movie
 
 const request = require('request');
+const movieId = process.argv[2];
 
-// Create an empty object to store character names
-const characterNames = {};
+const baseUrl = 'https://swapi.dev/api/films/';
 
-// Get the list of character URLs from the `/films/:id/` endpoint
-request.get('https://swapi.dev/api/films/3/characters', (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+request(baseUrl + movieId, { json: true }, (err, res, filmData) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const characterUrls = filmData.characters;
 
-  // Parse the JSON response
-  const characterUrls = JSON.parse(body).results;
-
-  // Fetch and print character names
-  fetchAndPrintCharacterNames(characterUrls);
-});
-
-// Function to fetch and print character names
-async function fetchAndPrintCharacterNames(characterUrls) {
-  // Iterate over the character URLs and fetch the character name for each URL
-  for (const characterUrl of characterUrls) {
-    // Make an HTTP GET request to the character URL
-    const response = await request.get(characterUrl);
-
-    // Check if the response status code is 200 OK
-    if (response.statusCode === 200) {
-      // Parse the JSON response from the API
-      const characterData = JSON.parse(response.body);
-
-      // Add the character name to the object
-      characterNames[characterUrl] = characterData.name;
-    } else {
-      console.error(error);
+    // Function to fetch character names and print them
+    function fetchAndPrintCharacterNames(urls, index = 0) {
+      if (index < urls.length) {
+        request(urls[index], { json: true }, (err, res, characterData) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log(characterData.name);
+            fetchAndPrintCharacterNames(urls, index + 1);
+          }
+        });
+      }
     }
-  }
 
-  // Once all character names have been fetched, print them to the console
-  for (const characterUrl in characterNames) {
-    console.log(characterNames[characterUrl]);
+    fetchAndPrintCharacterNames(characterUrls);
   }
-}
+});
