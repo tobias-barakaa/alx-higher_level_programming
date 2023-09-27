426db089bfb9d9fc5307169d54cb0bf7eab8f7f7
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 19:14:52 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,13 @@
 #!/usr/bin/node
-const request = require('request');
 // Extract the API URL from the command line argument
+const request = require('request');
 const apiUrl = process.argv[2];
-// Character ID for Wedge Antilles
-// Make a GET request to the API
 request(apiUrl, (error, response, body) => {
-  if(error)
-  {
-    console.log(error)
-  }
-
-  // Check if the response status code is 200 (OK)
-  if (response.statusCode === 200) {
-    try {
+ try {
       const filmsData = JSON.parse(body).results;
 
       // Initialize a count variable
       let count = 0;
-
-      // Loop through the films and count the occurrences of Wedge Antilles
       for (const film of filmsData) {
         if (film.characters.includes('/18/')) {
           count ++;
@@ -28,8 +17,5 @@ request(apiUrl, (error, response, body) => {
       console.log(count);
     } catch (error) {
       console.log(error);
-    }
-  } else {
-    console.log(response.statusCode);
-  }
+    } 
 });
