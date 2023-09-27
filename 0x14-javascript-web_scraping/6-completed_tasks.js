b00e72c3cb9b6a41271d38d834ab8a118951f11e
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-27 22:23:43 +0300

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,24 @@
-any
+const request = require('request');
+const apiUrl = process.argv[2];
+
+// Initialize an empty object to store completed task counts by user ID
+const completedTasksByUser = {};
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+const todos = JSON.parse(body);
+
+  // Iterate through the todos and count completed tasks by user
+  for (const todo of todos) {
+    if (todo.completed) {
+      const userId = todo.userId;
+      completedTasksByUser[userId] = (completedTasksByUser[userId] || 0) + 1;
+    }
+  }
+
+  // Print the completed tasks by user as a JSON object
+  console.log(completedTasksByUser);
+});
